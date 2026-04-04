cbc96e63c31d90b92cb3d1ed9f2be77d4aa4f564
Janos Vegh <vegh.janos@gmail.com>
2026-04-04 08:33:06 +0200

    Default 'heartbeat' length changed

--- code/neuron/include/scGenComp_Config.h
+++ code/neuron/include/scGenComp_Config.h
@@ -7,7 +7,7 @@
 #define HEARTBEAT_TIME_DEFAULT sc_core::sc_time(1000,SC_PS)
 #define HEARTBEAT_TIME_RESOLUTION sc_core::sc_time(100,SC_PS)
 //#define HEARTBEAT_TIME_RESOLUTION 10 //psec
-#define HEARTBEAT_TIME_DEFAULT_BIO sc_core::sc_time(5,SC_US)//usec
+#define HEARTBEAT_TIME_DEFAULT_BIO sc_core::sc_time(1,SC_US)//usec
 #define HEARTBEAT_TIME_RESOLUTION_BIO sc_core::sc_time(1,SC_US)
 
 

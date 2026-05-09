6dc4890496930187bda4adae268fa39d44a9d1a8
Janos Vegh <vegh.janos@gmail.com>
2026-05-09 14:17:56 +0200

    Prepared CLI handling

---
+++ code/src/cli/programutilities.h
@@ -0,0 +1,14 @@
+#pragma once
+
+//#include "assembler/program.h"
+#include <QFile>
+
+
+/*
+namespace Ripes {
+
+QString loadFlatBinaryFile(Program &program, const QString &filepath,
+                           unsigned long entryPoint, unsigned long loadAt);
+
+} // namespace Ripes
+  */
